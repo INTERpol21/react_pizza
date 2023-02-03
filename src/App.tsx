6135361612de6238f44bc5b1f50866c866b9dc40
Anton Zhuravlev <79113636+INTERpol21@users.noreply.github.com>
2023-02-03 22:22:05 +0600

    modified:   package-lock.json 	modified:   package.json 	modified:   public/index.html 	deleted:    src/App.css 	modified:   src/App.tsx 	new file:   src/assets/img/arrow-top.svg 	new file:   src/assets/img/cart.svg 	new file:   src/assets/img/empty-cart.png 	new file:   src/assets/img/grey-arrow-left.svg 	new file:   src/assets/img/pizza-logo.svg 	new file:   src/assets/img/plus.svg 	new file:   src/assets/img/trash.svg 	new file:   src/assets/pizzas.json 	new file:   src/components/Categories/Categories.tsx 	new file:   src/components/Header/Header.tsx 	new file:   src/components/PizzaBlock/PizzaBlock.tsx 	new file:   src/components/Sort/Sort.tsx 	new file:   src/scss/_variables.scss 	new file:   src/scss/app.scss 	new file:   src/scss/components/_all.scss 	new file:   src/scss/components/_button.scss 	new file:   src/scss/components/_categories.scss 	new file:   src/scss/components/_header.scss 	new file:   src/scss/components/_pizza-block.scss 	new file:   src/scss/components/_sort.scss 	new file:   src/scss/libs/_normalize.scss 	modified:   tsconfig.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,42 @@
-import "./App.css";
+import Header from './components/Header/Header';
+import './scss/app.scss';
+import Categories from './components/Categories/Categories';
+import Sort from './components/Sort/Sort';
+import PizzaBlock from './components/PizzaBlock/PizzaBlock';
+import pizzas from './assets/pizzas.json';
+type FormatDateProps = {
+  id: number;
+  imageUrl: string;
+  title: string;
+  types: number[];
+  sizes: number[];
+  price: number;
+  category: number;
+  rating: number;
+};
 
 function App() {
-  return <div className="App">
-    <h1>gdfdsfdsfsdfdsfds</h1>
-  </div>;
+  return (
+    <div className="App">
+      <div className="wrapper">
+        <Header />
+        <div className="content">
+          <div className="container">
+            <div className="content__top">
+              {/* <Categories /> */}
+              <Sort />
+            </div>
+            <h2 className="content__title">Все пиццы</h2>
+            <div className="content__items">
+              {pizzas.map((obj: FormatDateProps) => (
+                <PizzaBlock {...obj} key={obj.id} />
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
